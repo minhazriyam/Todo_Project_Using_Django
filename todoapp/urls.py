0fa107a12977c3b2d8f02c58bc899757fa25797e
minhazriyam <minhazriyam15@gmail.com>
2025-01-30 14:44:37 +0100

    Basic Functional Version

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -5,6 +5,20 @@ urlpatterns = [
  
     path("addTask/", views.addTask, name= 'addTask'),
 
+    # Mark as done
+    path("mark_as_done/<int:pk>/", views.mark_as_done, name = "mark_as_done"),
+    
+    # Mark as undone
+    path("mark_as_undone/<int:pk>/", views.mark_as_undone, name = "mark_as_undone"),
+
+    #Edit Task
+    path("editTask/<int:pk>/", views.editTask, name= "editTask"),
+
+    #Delete Task
+
+    path("deleteTask/<int:pk>/", views.deleteTask, name= "deleteTask")
+
+
 
 
 ]
