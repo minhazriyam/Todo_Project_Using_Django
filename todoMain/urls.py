42af9ce16a6a096c1450373e1f9e06652e8731bd
minhazriyam <minhazriyam15@gmail.com>
2025-01-30 11:49:38 +0100

    Updated Some Features

--- todoMain/urls.py
+++ todoMain/urls.py
@@ -16,11 +16,17 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
-from . import views
+from django.urls import path,include
+from . import views 
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", views.home, name= 'home')
+    path("", views.home, name= 'home'),
+
+    # TODO PATH
+
+    path("todoapp/", include("todoapp.urls")),
+
+
 ]
